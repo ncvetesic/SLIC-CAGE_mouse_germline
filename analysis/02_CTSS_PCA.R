107a9aee9f3f2503fff534b882c10701f8b4c9c8
Nevena <nevena.cvetesic@gmail.com>
2020-11-10 09:42:34 +0000

    Update 02_CTSS_PCA.R
    Finished code for CTSS PCA.

--- analysis/02_CTSS_PCA.R
+++ analysis/02_CTSS_PCA.R
@@ -71,80 +71,79 @@
   count.matrix <- as.matrix(raw.counts.df)
 
 
-#------- all merged data without TBP2 KO and P7
+#------- all merged data 
 # info table/ design table / colData
 # load raw CAGE set
-rawCAGEset_merged_reps <- readRDS(file = "../all_reps_PN6_2cell/intermediate_data/rawCAGEset_merged_reps.RDS")
+  rawCAGEset_merged_reps <- readRDS(file = "../all_reps_PN6_2cell/intermediate_data/rawCAGEset_merged_reps.RDS")
 
 ## Prepare the raw count table
-raw.counts.df <- data.frame(consensusClustersTpm(rawCAGEset_merged_reps))
+  raw.counts.df <- data.frame(consensusClustersTpm(rawCAGEset_merged_reps))
 
 # prepare DeSeq2 object
-library(DESeq2)
+  library(DESeq2)
 
 # create a count matrix for DESeq2
-count.matrix <- as.matrix(raw.counts.df)
+  count.matrix <- as.matrix(raw.counts.df)
 
-count.matrix.sel <- count.matrix[, -c(14, 15, 17)]
 
-samples <- colnames(count.matrix.sel)
-info.df <- data.frame(condition = factor(x = c("mESC-E14", rep("PGC", times = 11), rep("oocyte", times  = 3), rep("embryo", times = 2)), 
-                                         levels = c("mESC-E14", "PGC", "oocyte", "embryo")),
-                      row.names = samples)
+
+  samples <- colnames(count.matrix)
+  info.df <- data.frame(condition = factor(x = c("mESC-E14", rep("PGC", times = 11), rep("oocyte", times  = 3), rep("embryo", times = 2)), 
+                                           levels = c("mESC-E14", "PGC", "oocyte", "embryo")),
+                        row.names = samples)
 
 # create DESeqDataSet object - stores count matrix and design
-dds <- DESeqDataSetFromMatrix(countData = count.matrix.sel,
-                              colData = info.df,
-                              design = ~ condition)
+  dds <- DESeqDataSetFromMatrix(countData = count.matrix,
+                                colData = info.df,
+                                design = ~ condition)
 
-col <- c("gold1", "darkslateblue", "dodgerblue")
+  col <- c("gold1", "darkslateblue", "dodgerblue")
 
 
 # PCA preparation
-dds <- DESeq(dds)
-vsd <- vst(dds, blind = TRUE) # blind means don't take design into consideration
+  dds <- DESeq(dds)
+  vsd <- vst(dds, blind = TRUE) # blind means don't take design into consideration
 
 # save dds object PGC embryo oocyte  
-saveRDS(dds, "../all_reps_PN6_2cell/intermediate_data/dds_all_merg_no_TBP2.RDS")
+  saveRDS(dds, "../all_reps_PN6_2cell/intermediate_data/dds_all_merg_no_TBP2.RDS")
 
 # using top 10000 most variable tag clusters
-pcaData <- plotPCA(vsd, intgroup=c("condition"), ntop = 10000, returnData=TRUE)
-percentVar <- round(100 * attr(pcaData, "percentVar"))
+  pcaData <- plotPCA(vsd, intgroup=c("condition"), ntop = 10000, returnData=TRUE)
+  percentVar <- round(100 * attr(pcaData, "percentVar"))
 
 # plotting of PCA
-library(ggrepel)
+  library(ggrepel)
 
 # change names of samples and levels
-samples <- factor(c("mESC-E14", "E9.5", "E10.5", "E11.5", "E12.5F", "E12.5M", "E13.5F", "E13.5M", "E14.5F", "E14.5M", "E16.5F", "E16.5M", "P6", "P14", "MII", "2-cell", "4-cell"), levels = c("mESC-E14", "E9.5", "E10.5", "E11.5", "E12.5F", "E12.5M", "E13.5F", "E13.5M", "E14.5F", "E14.5M", "E16.5F", "E16.5M", "P6", "P14", "MII", "2-cell", "4-cell"))
-pcaData$name <- samples
+  samples <- factor(c("mESC-E14", "E9.5", "E10.5", "E11.5", "E12.5F", "E12.5M", "E13.5F", "E13.5M", "E14.5F", "E14.5M", "E16.5F", "E16.5M", "P6", "P14", "MII", "2-cell", "4-cell"), levels = c("mESC-E14", "E9.5", "E10.5", "E11.5", "E12.5F", "E12.5M", "E13.5F", "E13.5M", "E14.5F", "E14.5M", "E16.5F", "E16.5M", "P6", "P14", "MII", "2-cell", "4-cell"))
+  pcaData$name <- samples
 
 # setting plotting colour
-col <- col_scheme
+  col <- col_scheme
+
+  p <-ggplot(pcaData, aes(PC1, PC2, fill = name)) +
+    geom_point(size=3, pch = 21, colour = "black") +
+    scale_fill_manual(values = col) +
+    xlab(paste0("PC1: ",percentVar[1],"% variance")) +
+    ylab(paste0("PC2: ",percentVar[2],"% variance")) + 
+    coord_fixed() +
+    theme_bw() +
+    theme(text = element_text(size = 20, colour = "black"), 
+          axis.text.x = element_text(size = 18, colour = "black"),
+          axis.text.y = element_text(size = 18, colour = "black"),
+          legend.title = element_blank(),
+          legend.position = "none") + 
+    geom_label_repel(aes(label = name),
+                     fill = "white", colour = "grey50",
+                     box.padding   = 0.35, 
+                     point.padding = 0.5,
+                     segment.color = 'grey50',
+                     show.legend = F) +
+    scale_x_continuous(limits = c(-300, 500), breaks = seq(-300, 500, by = 100)) +
+    scale_y_continuous(limits = c(-200, 200), breaks = seq(-200, 200, by = 100))
 
 
-p <-ggplot(pcaData, aes(PC1, PC2, fill = name)) +
-  geom_point(size=3, pch = 21, colour = "black") +
-  scale_fill_manual(values = col) +
-  xlab(paste0("PC1: ",percentVar[1],"% variance")) +
-  ylab(paste0("PC2: ",percentVar[2],"% variance")) + 
-  coord_fixed() +
-  theme_bw() +
-  theme(text = element_text(size = 20, colour = "black"), 
-        axis.text.x = element_text(size = 18, colour = "black"),
-        axis.text.y = element_text(size = 18, colour = "black"),
-        legend.title = element_blank(),
-        legend.position = "none") + 
-  geom_label_repel(aes(label = name),
-                   fill = "white", colour = "grey50",
-                   box.padding   = 0.35, 
-                   point.padding = 0.5,
-                   segment.color = 'grey50',
-                   show.legend = F) +
-  scale_x_continuous(limits = c(-300, 500), breaks = seq(-300, 500, by = 100)) +
-  scale_y_continuous(limits = c(-200, 200), breaks = seq(-200, 200, by = 100))
 
-
-
-pdf(file = "Figures/PCA_merged_rep_mESC_PGCs_oocyte_no_P7_2-cell_embryo.pdf", height = 8, width = 6)
-print(p)
-dev.off()
+  pdf(file = "Figures/PCA_merged_rep_mESC_PGCs_oocyte_embryo.pdf", height = 8, width = 6)
+    print(p)
+  dev.off()
